97e0f23c95eef111590db42db33e30d919c38cd2
Kabiswa Davis <kabiswad@gmail.com>
2023-03-06 23:51:21 +0200

    set up routes

--- server/index.js
+++ server/index.js
@@ -2,12 +2,17 @@ import express from "express";
 import * as dotenv from 'dotenv';
 import cors from 'cors';
 import connnectDb from "./mongodb/connect.js";
+import postRoutes from './routes/postRoutes.js';
+import dalleRoutes from './routes/dalleRoutes.js';
 
 dotenv.config()
 
 const app = express()
 app.use(cors())
 app.use(express.json({limit: '50mb'}))
+
+app.use('/api/vi/post', postRoutes)
+app.use('/api/vi/dalle', dalleRoutes)
 
 app.get('/', async (req, res) => {
     res.send('Hello from DALL-E clone')
---
+++ server/routes/dalleRoutes.js
@@ -0,0 +1,6 @@
+import express from "express";
+import * as dotenv from "dotenv"
+import { Configuration, OpenApi } from 'openai'
+
+dotenv.config()
+const router = express.Router()
